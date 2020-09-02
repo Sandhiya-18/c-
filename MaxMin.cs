47c4bdc9f45af8d0e67c584130ca9ff9c84173fc
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 18:56:28 +0530

    Add files via upload

---
+++ MaxMin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MaxMin
+{
+    class MaxMin
+    {
+        static void Main(string[] args)
+        {
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integer is {min} to {max}");
+
+            double max1 = double.MaxValue;
+            double min1 = double.MinValue;
+            Console.WriteLine($"The range of integer is {min1} to {max1}");
+        }
+    }
+}
