ca54da68076484bb852789cebdeb9cb11ec865d7
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 15:52:09 +0530

    Add files via upload

---
+++ Replace.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Replace
+{
+    class Replace
+    {
+        static void Main(string[] args)
+        {
+            String one = "Hello World!";
+            Console.WriteLine(one);
+
+            one = one.Replace("Hello", "Greetings");
+            Console.WriteLine(one);
+        }
+    }
+}
