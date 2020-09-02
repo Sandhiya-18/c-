4fd077dd6b18c4d5cf97665044dacc82227e1ede
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 15:43:47 +0530

    Add files via upload

---
+++ Trim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Trim
+{
+    class Trim
+    {
+        static void Main(string[] args)
+        {
+            String Name = "     sandhiya     ";
+            Console.WriteLine($"[***{Name}***]");
+            String Name2 = Name.TrimStart();
+            Console.WriteLine($"[***{Name2}***]");
+            String Name3 = Name.TrimEnd();
+            Console.WriteLine($"[***{Name3}***]");
+            String Name4 = Name.Trim();
+            Console.WriteLine($"[***{Name4}***]");
+
+        }
+    }
+}
