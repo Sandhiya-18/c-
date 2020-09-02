10e1c53a5d160631e60371a2a769b2df848fe4bd
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 20:09:13 +0530

    Add files via upload

---
+++ IfElsecondition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ifcondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6;
+            int b = 6;
+            if(a + b > 10 && a==b)
+            
+            {
+                Console.WriteLine("The result is greater than 10");
+                Console.WriteLine("The first no. is equals to the second no.");
+
+            }
+            else
+            {
+                Console.WriteLine("The result is not greater than 10");
+                Console.WriteLine(" or The first no. is not equals to the second no.");
+                
+            }
+        }
+    }
+}
