8b1c297f1b1b5ca5cce77ffd527ca0ceeff74f32
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 17:10:51 +0530

    Add files via upload

---
+++ ArithOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Airth_numbers
+{
+    class Arithematics
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 8;
+            int c = 4;
+            int i = (a + b) * c + a * (c * a);
+            Console.WriteLine(i);
+        }
+    }
+}
