48c3fe82bde8a123ab113dfca91a2fffcf7dd9c4
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 15:19:08 +0530

    Add files via upload

---
+++ StringLength.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strings
+{
+    class StringLength
+    {
+        static void Main(string[] args)
+        {
+            String Name1 = "Akshay";
+            String Name2 = "Ishwarya";
+            Console.WriteLine($"The Name {Name1} has {Name1.Length} letters");
+            Console.WriteLine($"The Name {Name2} has {Name2.Length} letters");
+        }
+    }
+}
