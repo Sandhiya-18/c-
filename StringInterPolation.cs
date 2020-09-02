0a3bf745c011c0ecb946759f281f637ae18711ff
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 14:15:32 +0530

    Add files via upload

---
+++ StringInterPolation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Interpolation
+{
+	public Interpolation()
+	{
+		static void Main(string[] args)
+        {
+			String Name = "Sandhiya";
+			Console.WritrLine($"Hello {Name}");
+        }
+	}
+}
