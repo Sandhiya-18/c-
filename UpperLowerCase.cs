723414b13cb571e9627436e88e91bda26e5f5477
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 16:09:35 +0530

    Add files via upload

---
+++ UpperLowerCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ULcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String one = "Hello World!";
+            Console.WriteLine(one);
+
+            one = one.Replace("Hello", "Greetings");
+            Console.WriteLine(one);
+            Console.WriteLine(one.ToLower());
+            Console.WriteLine(one.ToUpper());
+        }
+    }
+}
