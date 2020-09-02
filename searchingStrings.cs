4fdc835accc9a7dfff15301834d60ec585aa29a3
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 16:36:04 +0530

    Add files via upload

---
+++ searchingStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StringSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Song = "My Last made me feel like i will never try again";
+            Console.WriteLine(Song.Contains("Last"));
+            Console.WriteLine(Song.Contains("like"));
+            Console.WriteLine(Song.StartsWith("like"));
+            Console.WriteLine(Song.StartsWith("My"));
+
+        }
+    }
+}
