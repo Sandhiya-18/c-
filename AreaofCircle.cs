8d6b744c1ae0b1c2b738b8a4000ce4da2a915a27
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-02 19:38:17 +0530

    Add files via upload

---
+++ AreaofCircle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 3.9;
+            double area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+        }
+    }
+}
