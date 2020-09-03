e786a4564b0976c02b69fc4c0683aa80e9c94d2a
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 12:05:43 +0530

    Add files via upload

---
+++ Dowhileloop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 20;
+
+            do
+            {
+                Console.WriteLine("The number is greater than 10");
+                count++;
+            }
+            while (count < 10);
+        }
+    }
+}
