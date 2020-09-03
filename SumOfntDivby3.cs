073adb3d5e2d975b6048527a35487717cb0bb31a
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 12:43:43 +0530

    Add files via upload

---
+++ SumOfntDivby3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Loops2
+{
+    class Class1
+    {
+        static void Main(String[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if(i%3 == 0)
+                {
+                    sum = sum + i;
+                }
+
+            }
+            Console.WriteLine($"The sum is {sum}.");
+        }
+    }
+}
