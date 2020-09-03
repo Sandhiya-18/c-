c41f380c16f7cd6068f5a4cff7ec551649dbd1ef
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 18:40:07 +0530

    Add files via upload

---
+++ MINIBANKPROJECT/BankAccount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        public string AccNumber { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in alltransaction)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+
+
+            
+            }
+        }
+        private static int accountnumseed = 1234567890;
+
+        private List<Transaction> alltransaction = new List<Transaction>();
+        public BankAccount(String name, decimal initialbalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialbalance, DateTime.Now, "initialbalance");
+            this.AccNumber = accountnumseed.ToString();
+            accountnumseed++;
+        }
+        public void MakeDeposit(decimal amount, DateTime date,String notes)
+        {
+            if(amount<=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of Deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, notes);
+            alltransaction.Add(deposit);
+
+        }
+        public void MakeWithDrawal(decimal amount, DateTime date, String notes)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, notes);
+            alltransaction.Add(withdrawal);
+        }
+
+           public string GetAccountHistory()
+            {
+            var report = new StringBuilder();
+            //Header
+            report.AppendLine("Date \t\t \t\tAmount \t\t\t\t Notes");
+                foreach(var j in alltransaction)
+            {
+                report.AppendLine($"{j.Date} \t\t {j.Amount} \t\t\t {j.Notes}");
+            }
+            return report.ToString();
+            }
+
+            
+        
+
+    }
+}
---
+++ MINIBANKPROJECT/Main.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("sandhiya", 100000);
+            Console.WriteLine($"Account {account.AccNumber} was created for {account.Owner} with {account.Balance} balance.");
+
+            account.MakeWithDrawal(2000, DateTime.Now, "akshay");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+
+            //test that the  initial balance must be positive
+
+            try
+            {
+                var invalidamount = new BankAccount("invaliduser", -100);
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine("Exception caught while creating account with negative balance. ");
+                Console.WriteLine(e.ToString());
+            }
+            account.MakeWithDrawal(2000, DateTime.Now, "akshay");
+            Console.WriteLine(account.Balance);
+
+
+
+        }
+
+    }
+}
