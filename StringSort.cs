2ca6c3aa276ab5cc1874bae4c8abc348a044bb58
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 13:59:55 +0530

    Add files via upload

---
+++ StringSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loops2
+{
+    class Class1
+    {
+        static void Main(String[] args)
+        {
+            var animals = new List<string> { "monkey", "tiger", "lion", "cheeta" };
+
+            animals.Add("dog");
+            animals.Add("cat");
+            animals.Remove("monkey");
+            animals.Add("elephant");
+            animals.Sort();
+            foreach (String i in animals)
+            {
+                Console.WriteLine($"Hey its a {i.ToUpper()}");
+            }
+
+            var index = animals.IndexOf("elephant");
+            Console.WriteLine($"Found the index of elephant at {index}");
+        }
+        }
+    }
