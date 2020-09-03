8ac46bdbdfa390978f81bd69460da3346a156056
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 13:38:47 +0530

    Add files via upload

---
+++ Listofstrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loops2
+{
+    class Class2
+    {
+        static void Main(String[] args)
+        {
+            var animals = new List<string> { "monkey", "tiger", "lion", "cheeta" };
+
+            animals.Add("dog");
+            animals.Add("cat");
+            animals.Remove("monkey");
+            animals.Add("elephant");
+            foreach(String i in animals)
+            {
+                Console.WriteLine($"Hey its a {i.ToUpper()}");
+            }
+
+            Console.WriteLine(animals[3]);
+
+        }
+
+    }
+}
