ad4225805ca7868b1bb85681221527862ab19cac
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 14:26:58 +0530

    Add files via upload

---
+++ fibbacciseries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loops2
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            var fibbo = new List<int> { 1, 1 };
+
+            while (fibbo.Count < 20)
+            {
+                var previous1 = fibbo[fibbo.Count - 1];
+                var previous2 = fibbo[fibbo.Count - 2];
+                fibbo.Add(previous2 + previous1);
+            }
+            foreach (var i in fibbo)
+                {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
