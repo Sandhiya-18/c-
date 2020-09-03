6cfecfcc02e949636a69576858c695094515ab04
Sandhiya-18 <70628255+Sandhiya-18@users.noreply.github.com>
2020-09-03 12:18:02 +0530

    Add files via upload

---
+++ forloop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+
+                Console.WriteLine("The number is lesser than 10");
+
+            }
+        }
+    }
+}
